9a35f89c061a0ffa634d31aae7f2eea1e32f3501
Apex <gonz.anthony6@gmail.com>
2022-07-16 19:08:15 -0700

    namespaces

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
+namespace first {
+    int x = 1;
+}
+namespace second{
+    int x = 2;
+}
+
 int main(){
-    //This is a comment
-    std::cout << "I like guac!" << '\n';
-    std::cout << "Fucking hell dude!" << '\n';
+    using namespace std;
+    /*Namespace: 
+    provides a solution for preventing name conflicts in a
+    large project. Each entity needs a unique name. A namespace
+    allows for identically named entities as long as the namespaces are different
+    */ 
+   string name = "Bro";
+
+   cout << name; //:: scope resolution operator
+
     return 0;
 }
