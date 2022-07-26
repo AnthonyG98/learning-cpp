452559b4b5df1440d712b7625675d02437cbe9ef
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-07-26 15:27:58 -0700

    Strings

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
 
 int main () {
-    double temp;
-    char unit;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
 
-    std::cout << "****** Temp conversion";
-    std::cout << "F = Farneheit\n";
-    std::cout << "C = Celcius\n";
-    std::cout << "What unit would you like to convert to?";
 
-    std::cin >> unit;
 
-    if(unit =='F' || unit =='f') {
-        std::cout << "Enter the Temperature in Celcius: ";
-        std::cin >> temp;
-        temp = (1.8 * temp) + 32.0;
-        std::cout << "Temperature is "<< temp << "F\n";
-    } else if(unit == 'C' || unit == 'c'){
-        std::cout << "Enter the Temperature in Farenheit: ";
-        std::cin >> temp;
-        temp = (temp - 32) / 1.8;
-        std::cout << "Temperature is "<< temp << "C\n";
-    } else {
-        std::cout << "Please enter only C or F \n";
-    }
-
-    std::cout << "************ \n";
+    std::cout << "Hello " << name.erase(0, 2);
 
     return 0;
 }
