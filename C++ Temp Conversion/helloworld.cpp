b095defa290feb29886589aa5e6fd28b7be1670f
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-07-27 15:08:10 -0700

    Do/While (dowap) loop

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
 int main () {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-
-
-
-    std::cout << "Hello " << name.erase(0, 2);
+    int number;
+    do{
+        std::cout << "Enter a positive number";
+        std::cin >> number;
+    } while (number < 0);
+    
+    std::cout << "The number is " << number;
 
     return 0;
 }
