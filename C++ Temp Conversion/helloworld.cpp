96403af7ed087bd24f0a00aad544d72f55815e80
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-04 15:51:36 -0700

    Unfinished simple bank

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 
-std::string concatStrings (std::string stringOne, std::string stringTwo);
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
 int main () {
+    double balance = 0;
+    int choice = 0;
+   do {
+        std::cout << "******************* \n";
+        std::cout << "Enter your choice \n";
+        std::cout << "******************* \n";
 
-    std::string firstName = "Anthony";
-    std::string lastName = "Gonzo";
-    std::string fullName = concatStrings(firstName, lastName);
+        std::cout << "1. Show Balance \n";
+        std::cout << "2. Make a withdrawl \n";
+        std::cout << "3. Make a deposit \n";
+        std::cout << "4. Exit \n";
 
-    std::cout << fullName;
+        std::cin >> choice;
+    
+        switch(choice){
+            case 1: showBalance(balance);
+                break;
+            case 2: balance -= withdraw(balance);
+                break;
+            case 3: balance += deposit();
+                break;
+            case 4: std::cout << "Thanks for stopping by! \n";
+                break;
+            default: std::cout << "Invalid choice";
+        }
+   }
+    while(choice != 4);
 
     return 0;
 }
-std::string concatStrings (std::string stringOne, std::string stringTwo){
-    return stringOne + " " + stringTwo;
-}
+
