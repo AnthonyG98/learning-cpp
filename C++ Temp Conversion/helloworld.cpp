b58199047ed20a33d60b1c7e4578a2dbc4844583
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-05 12:01:34 -0700

    Simple banking system

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+#include <iomanip>
 void showBalance(double balance);
 double deposit();
 double withdraw(double balance);
 
-int main () {
+int main () { 
     double balance = 0;
     int choice = 0;
    do {
@@ -23,8 +23,10 @@ int main () {
             case 1: showBalance(balance);
                 break;
             case 2: balance -= withdraw(balance);
+                        showBalance(balance);
                 break;
             case 3: balance += deposit();
+                        showBalance(balance);
                 break;
             case 4: std::cout << "Thanks for stopping by! \n";
                 break;
@@ -36,3 +38,29 @@ int main () {
     return 0;
 }
 
+void showBalance(double balance){
+    std::cout << "Your balance is $"<< std::setprecision(2) <<std::fixed << balance << '\n';
+}
+double deposit(){
+    double amount = 0;
+    std::cout << "Enter amount to deposit: ";
+    std::cin >> amount;
+       if(amount > 0){
+        return amount;
+        } else {
+            std::cout << "Not a valid amount";
+            return 0;
+        }
+}   
+double withdraw(double balance){
+    double withdrawl = 0;
+    std::cout << "Enter amount you'd like to withdrawl: ";
+    std::cin >> withdrawl;
+    if(withdrawl > balance){
+        std::cout << "Insufficient funds!";
+        return 0;
+    } else {
+        return withdrawl;
+    }
+    return 0;
+}
