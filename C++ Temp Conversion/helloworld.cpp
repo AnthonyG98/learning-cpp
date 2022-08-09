5cfd22a14ddbb8fbe469c7a19ed41b297da25372
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-09 13:11:43 -0700

    Fill function

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
 
-void sort(int array[], int size);
 int main () { 
 
-    int array [] = {2, 5, 7, 1, 3, 9, 4, 10, 6, 8};
-    int size = sizeof(array)/sizeof(array[0]);
+    const int size = 99;
+    std::string foods[size];
 
-    sort(array, size);
+    fill(foods, foods + (size / 3) , "pizza");
+    fill(foods + (size / 3), foods + (size / 3) * 2, "hamburger");
+    fill(foods + (size / 3) * 2, foods + size, "hotdogs");
 
-    for(int element : array){
-        std::cout << element << " ";
+
+    for(std::string food : foods){
+        std::cout << food << '\n';
     }
 
     return 0;
-}
-
-void sort(int array[], int size){
-
-    int temp;
-
-    for(int i = 0; i < size -1; i++){
-        for(int j = 0; j < size - i - 1; j++){
-            if(array[j] > array[j +1]){
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-
 }
