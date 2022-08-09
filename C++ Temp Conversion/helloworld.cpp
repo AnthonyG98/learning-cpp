bd8c8b0c10f78e65b2b71bbfa118600f7ea757c9
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-08 21:10:38 -0700

    Sorting an array

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-int searchArray(int array[], int size, int element);
+void sort(int array[], int size);
 int main () { 
 
-    int numbers[] = { 1, 2, 3, 4, 5 };
-    int size = sizeof(numbers)/sizeof(numbers[0]);
-    int index;
-    int myNum;
-    std::cout << "enter element to search for: ";
-    std::cin >> myNum;
-    
-    index = searchArray(numbers, size, myNum);
-    
-    if(index != -1){
-        std::cout << myNum <<" is at " << index;
-    } else {
-        std::cout << myNum << " was not found";
-        std::cout <<numbers;
+    int array [] = {2, 5, 7, 1, 3, 9, 4, 10, 6, 8};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    sort(array, size);
+
+    for(int element : array){
+        std::cout << element << " ";
     }
+
     return 0;
 }
 
-int searchArray(int array[], int size, int element){
-    for(int i = 0; i < size; i++){
-        if(array[i] == element){
-            return i;
+void sort(int array[], int size){
+
+    int temp;
+
+    for(int i = 0; i < size -1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            if(array[j] > array[j +1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
     }
-        return -1;
+
 }
