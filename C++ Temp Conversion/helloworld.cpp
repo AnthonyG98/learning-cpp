2a8236dc28c579cea67add9e25b2e54bb24630a5
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-11 14:02:53 -0700

    C++ Quiz game

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -2,17 +2,42 @@
 
 int main () { 
 
-    const int size = 99;
-    std::string foods[size];
+    std::string questions[] = {
+        "1. What year was C++ created?: ",
+        "2. Who invented C++?: ",
+        "3. What is the predecessor of C++?: ",
+        "4. Is the earth flat?: "
+    };
+    std::string options[][4] = {
+        {
+            "A. 1969", "B. 1975", "C. 1985", "D. 1989"
+        },
+        {
+            "A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg"
+        },
+        {
+            "A. C", "B. C+", "C. C--", "D. B++"
+        },
+        {
+            "A. Yes", "B. No", "C. Sometimes", "D. What is earth?"
+        }
+    };
 
-    fill(foods, foods + (size / 3) , "pizza");
-    fill(foods + (size / 3), foods + (size / 3) * 2, "hamburger");
-    fill(foods + (size / 3) * 2, foods + size, "hotdogs");
+    char answerKey[] = {'C', 'B', 'A', 'B'};
 
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
 
-    for(std::string food : foods){
-        std::cout << food << '\n';
-    }
+    for(int i = 0; i < size; i++){
+        std::cout << "****************** \n";
+        std::cout << questions[i] << '\n';
+        std::cout << "****************** \n";
+
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
+        };
+    };
 
     return 0;
 }
