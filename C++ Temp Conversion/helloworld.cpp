6edf7b31b14e35f1a028b7ecfdae6099197704a4
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-15 19:39:19 -0700

    Dynamic memory

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
-int getDigit(const int number);
-int sumOddDigits(const std::string cardNumber);
-int sumEvenDigits(const std::string cardNumber);
 
 int main () { 
+    /*
+    Dynamic memory = Memory that is allocated after the program is already 
+    compiled and running. Use the 'new' operator to allocate memory in the heap
+    rather than the stack
 
-    std::string cardNumber;
-    int result = 0;
+    Useful when we dont know how much memory we will need. Makes our programs 
+    more flexible especially when accepting user input.
+    */
 
-    std::cout << "Enter a credit card #:";
-    std::cin >> cardNumber;
+    char *pGrades = NULL;
+    int size;    
+    
+    std::cout << "how many Grades to enter in?:";
+    std::cin >> size;
 
-    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+    pGrades = new char[size];
 
-    if(result % 10 == 0){
-        std::cout << cardNumber << " is valid";
-    } else {
-        std::cout << cardNumber << " is not valid";
+    for(int i = 0; i < size; i++){
+        std::cout << "Enter grade #" << i +1 << ": ";
+        std::cin >> pGrades[i];
     }
+    for(int i = 0; i < size; i++){
+        std::cout << pGrades[i] << " ";
+    }
+
+    delete[] pGrades;
+
     return 0;
-}
-int getDigit(const int number){
-
-
-
-    return number % 10 + (number / 10 % 10);
-}
-int sumOddDigits(const std::string cardNumber){
-    int sum = 0;
-
-    for(int i = cardNumber.size() - 1; i >= 0; i-=2){
-        sum += getDigit(cardNumber[i] - '0');
-    }
-    return sum;
-}
-int sumEvenDigits(const std::string cardNumber){
-    int sum = 0;
-
-    for(int i = cardNumber.size() - 2; i >= 0; i-=2){
-        sum += getDigit((cardNumber[i] - '0') * 2);
-    }
-    return sum;
 }
