4900a124fbb089a08ff87a81d1047be3b5bd1660
Anthony Gonzalez <gonz.anthony6@gmail.com>
2022-08-18 08:24:00 -0700

    Structs

--- C++ Temp Conversion/helloworld.cpp
+++ C++ Temp Conversion/helloworld.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
-template <typename T, typename U>
-
-auto max(T x, U y){
-    return(x > y) ? x : y;
+/*
+struct = A structure that group related variables under one name 
+            structs can contain many diffrent data types (strings, int, double)
+            variables in a struct are known as "members".
+            vaiables in a struct can be accessed with '.' "Class member Access Operator".
+*/
+struct student {
+    std::string name;
+    double gpa;
+    bool enrolled;
 };
 int main () { 
-    /*
-    function templates = describes what a function looks like. Can be used
-    to generate as many overloaded functions as needed, each using different data
-    types.
-    */
-   std::cout << max(1, 2) << '\n';
+    student studentOne;
+    studentOne.name = "Spongebob";
+    studentOne.gpa = 3.4;
+    studentOne.enrolled = true;
+
+    student studentTwo;
+    studentTwo.name = "Patrick";
+    studentTwo.gpa = 3.0;
+    studentTwo.enrolled = true;
+
+    std::cout << studentOne.name << '\n';
+    std::cout << studentOne.gpa << '\n';
+    std::cout << studentOne.enrolled << '\n';
+
+    std::cout << studentTwo.name << '\n';
+    std::cout << studentTwo.gpa << '\n';
+    std::cout << studentTwo.enrolled << '\n';
+
     return 0;
 }
